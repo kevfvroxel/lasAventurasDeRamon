af8395f89b330f7775060e7045c850022d08fb08
7beelzebub7 <rc0870984@gmail.com>
2025-05-22 18:49:58 -0500

    cambios en la animación de salto

--- game.js
+++ game.js
@@ -3,6 +3,7 @@
 import { createAnimations } from "./animations.js"
 
 const config = {
+  autoFocus: false,
   type: Phaser.AUTO, // webgl, canvas
   width: 1920,
   height: 900,
@@ -42,6 +43,11 @@ function preload () {
   )
 
   this.load.spritesheet(
+    'goomba',
+    'assets/entities/overworld/goomba.png',
+    { frameWidth: 16, frameHeight: 16 }
+  )
+  this.load.spritesheet(
     'mario', // <--- id
     'assets/entities/mario.png',
     { frameWidth: 18, frameHeight: 16 }
@@ -59,26 +65,40 @@ function create () {
   this.floor = this.physics.add.staticGroup()
 
   this.floor
-    .create(0, config.height - 16, 'floorbricks')
+    .create(0, config.height - 32, 'floorbricks')
     .setOrigin(0, 0.5)
+    .setScale(2)
     .refreshBody()
 
   this.floor
-    .create(150, config.height - 16, 'floorbricks')
+    .create(350, config.height - 32, 'floorbricks')
     .setOrigin(0, 0.5)
+    .setScale(2)
     .refreshBody()
 
   this.floor
-    .create(200, config.height - 56, 'pipe1')
+    .create(460, config.height - 112, 'pipe1')
     .setOrigin(0.5, 0.5)
+    .setScale(2)
     .refreshBody()
+
+  
+
     
-   //voy a poner mi piso del castillo xd xD Xd XD borren eso  baneen a manu
+   //baneen a manu
 
   this.mario = this.physics.add.sprite(50, 100, 'mario')
     .setOrigin(0, 1)
     .setCollideWorldBounds(true)
     .setGravityY(300)
+    .setScale(2)
+
+  this.enemy = this.physics.add.sprite(120, - 64, 'goomba')
+    .setScale(2)
+    .setOrigin(0,1)
+    .setGravityY(300)
+    .setGravityX(-50)
+  
 
   this.physics.world.setBounds(0, 0, 2000, config.height)
   this.physics.add.collider(this.mario, this.floor)
@@ -96,19 +116,19 @@ function update () { // 3. continuamente
   if (this.mario.isDead) return
 
   if (this.keys.left.isDown) {
-    this.mario.anims.play('mario-walk', true)
+    this.mario.body.touching.down && this.mario.anims.play('mario-walk', true)
     this.mario.x -= 2
     this.mario.flipX = true
   } else if (this.keys.right.isDown) {
-    this.mario.anims.play('mario-walk', true)
+    this.mario.body.touching.down && this.mario.anims.play('mario-walk', true)
     this.mario.x += 2
     this.mario.flipX = false
-  } else {
+  } else if(this.mario.body.touching.down){
     this.mario.anims.play('mario-idle', true)
   }
 
   if (this.keys.up.isDown && this.mario.body.touching.down) {
-    this.mario.setVelocityY(-300)
+    this.mario.setVelocityY(-400)
     this.mario.anims.play('mario-jump', true)
   }
 
