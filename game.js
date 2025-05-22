1f63adb70563ce1d60bf55af0e631a8d8806088b
kevfvroxel <cuenekevin@gmail.com>
2025-05-21 21:51:50 -0500

    actializacion del inicio

--- game.js
+++ game.js
@@ -4,14 +4,14 @@ import { createAnimations } from "./animations.js"
 
 const config = {
   type: Phaser.AUTO, // webgl, canvas
-  width: 256,
-  height: 244,
+  width: 1920,
+  height: 900,
   backgroundColor: '#049cd8',
   parent: 'game',
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
+      gravity: { y: 100 },
       debug: false
     }
   },
@@ -41,6 +41,37 @@ function preload () {
     'assets/entities/mario.png',
     { frameWidth: 18, frameHeight: 16 }
   )
+  //Agregamos este castillo
+  this.load.image(
+    'castle',
+    'assets/scenery/castle.png'
+
+  )
+  this.load.image(
+    'mastil',
+    'assets/scenery/flag-mast.png'
+  )
+    this.load.image(
+    'bandera',
+    'assets/scenery/final-flag.png',
+    
+  )
+  this.load.image(
+    'boquemisterioso',
+    'assets/blocks/overworld/misteryBlock.png'
+  )
+  this.load.image(
+    'arbusto',
+    'assets/scenery/overworld/bush1.png'
+  )
+   this.load.image(
+    'block',
+    'assets/blocks/overworld/block.png'
+  )
+    this.load.image(
+    'pipe1',
+    'assets/scenery/pipe1.png'
+  )
 
   this.load.audio('gameover', 'assets/sound/music/gameover.mp3')
 } // 1.
@@ -54,19 +85,132 @@ function create () {
   this.floor = this.physics.add.staticGroup()
 
   this.floor
+    .create(1250, config.height - 97, 'pipe1')
+    .setOrigin(0.5, 0.5)
+    .setScale(2)
+    .refreshBody()
+
+  this.floor
     .create(0, config.height - 16, 'floorbricks')
     .setOrigin(0, 0.5)
+     .setScale(2)
     .refreshBody()
 
   this.floor
     .create(150, config.height - 16, 'floorbricks')
     .setOrigin(0, 0.5)
+     .setScale(2)
     .refreshBody()
+//agegamos dos pisos 
+  this.floor
+    .create(1550, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+    .refreshBody()
+     .setScale(2)
+  this.floor
+    .create(1679, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+     .setScale(2)
+    .refreshBody()
+   this.floor
+    .create(479, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+     .setScale(2)
+    .refreshBody()
+     this.floor
+    .create(720, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+     .setScale(2)
+    .refreshBody()
+     this.floor
+    .create(900, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+     .setScale(2)
+    .refreshBody()
+    this.floor
+    .create(1180, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+     .setScale(2)
+    .refreshBody()
+  
+  //agregamos cstillo
+  this.add.image(1750,692,'castle')
+    .setOrigin(0, 0)
+    .setScale(2)
+  //agregamos bandera final y mastil
+  this.add.image(1680,685,'mastil')
+  .setScale(2)
+  this.add.image(1667,552,'bandera')
+  .setScale(2)
+//agrego bloque misterioso 
+ this.add.image(200,800,'boquemisterioso')
+  .setScale(2)
+//agrego pasto
+ this.add.image(100,835,'arbusto')
+  .setScale(1)
 
+  this.add.image(660, 836, 'block')
+  .setScale(2)
+    this.add.image(690, 806, 'block')
+  .setScale(2)
+    this.add.image(720, 775, 'block')
+  .setScale(2)
+    this.add.image(750, 745, 'block')
+  .setScale(2)
+
+
+    this.add.image(690, 836, 'block')
+  .setScale(2)
+    this.add.image(720, 806, 'block')
+  .setScale(2)
+    this.add.image(750, 775, 'block')
+  .setScale(2)
+    this.add.image(780, 745, 'block')
+  .setScale(2)
+
+    this.add.image(720, 836, 'block')
+  .setScale(2)
+    this.add.image(750, 806, 'block')
+  .setScale(2)
+    this.add.image(780, 775, 'block')
+  .setScale(2)
+    this.add.image(810, 745, 'block')
+  .setScale(2)
+  
+
+    this.add.image(750, 836, 'block')
+  .setScale(2)
+    this.add.image(780, 806, 'block')
+  .setScale(2)
+    this.add.image(810, 775, 'block')
+  .setScale(2)
+
+
+      this.add.image(780, 836, 'block')
+  .setScale(2)
+    this.add.image(810, 806, 'block')
+  .setScale(2)
+    this.add.image(840, 775, 'block')
+  .setScale(2)
+
+        this.add.image(810, 836, 'block')
+  .setScale(2)
+    this.add.image(840, 806, 'block')
+  .setScale(2)
+     this.add.image(870, 806, 'block')
+  .setScale(2)
+
+          this.add.image(840, 836, 'block')
+  .setScale(2)
+          this.add.image(870, 836, 'block')
+  .setScale(2)
+          this.add.image(900, 836, 'block')
+  .setScale(2)
   this.mario = this.physics.add.sprite(50, 100, 'mario')
     .setOrigin(0, 1)
     .setCollideWorldBounds(true)
     .setGravityY(300)
+    .setScale(2)
 
   this.physics.world.setBounds(0, 0, 2000, config.height)
   this.physics.add.collider(this.mario, this.floor)
@@ -80,6 +224,34 @@ function create () {
 }
 
 function update () { // 3. continuamente
+
+
+  if (this.mario.isDead) return
+
+  if (this.keys.left.isDown) {
+    this.mario.body.touching.down && this.mario.anims.play('mario-walk', true)
+    this.mario.x -= 2
+    this.mario.flipX = true
+  } else if (this.keys.right.isDown) {
+    this.mario.body.touching.down && this.mario.anims.play('mario-walk', true)
+    this.mario.x += 2
+    this.mario.flipX = false
+  } else if(this.mario.body.touching.down){
+    this.mario.anims.play('mario-idle', true)
+  }
+
+  if (this.keys.up.isDown && this.mario.body.touching.down) {
+    this.mario.setVelocityY(-400)
+    this.mario.anims.play('mario-jump', true)
+  }
+
+  if (this.mario.y >= config.height) {
+    this.mario.isDead = true
+    this.mario.anims.play('mario-dead')
+    this.mario.setCollideWorldBounds(false)
+    this.sound.add('gameover', { volume: 0.2 }).play()
+	}
+
   if (this.mario.isDead) return
 
   if (this.keys.left.isDown) {
